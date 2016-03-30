c0f19f9ef7c278546ad9bca08f44624f359f5fe4
hannoman <hannoman2@googlemail.com>
2016-03-30 04:06:18 +0200

    - "BPlusInstantiateTest" - different nonsense

--- playground/xxl_dk/BPlusInstantiateTest.java
+++ playground/xxl_dk/BPlusInstantiateTest.java
@@ -1,5 +1,11 @@
 package xxl_dk;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Function;
+
+import xxl.core.collections.containers.Container;
+import xxl.core.collections.containers.io.BlockFileContainer;
 import xxl.core.indexStructures.BPlusTree;
 
 /** Trying to instantiate a BPlus-Tree. A small step for mankind, but a ... */
@@ -14,13 +20,37 @@ public class BPlusInstantiateTest {
 	 */
 	static int BLOCK_SIZE = 1024;
 
-	public static void main(String[] args) {
+	public static void createTree(String[] args) {
+		
+		String container_prefix = null;
+		if(args.length > 0) { // custom container file
+			container_prefix = args[0];			
+		}
+		else { // std container file
+			String container_file_prefix = "simple_bplus_tree_test";
+			System.out.println("No filename as program parameter found. Using standard: \""+ "<project dir>\\temp_data\\"+ container_file_prefix +"\"");
+			
+			// and the whole thing in short
+			Path curpath = Paths.get("").toAbsolutePath();
+			if(!curpath.resolve("temp_data").toFile().exists()) {
+				System.out.println("Error: Couldn't find \"test_data\" directory.");
+				return;
+			}
+			container_prefix = curpath.resolve("temp_data").resolve(container_file_prefix).toString();
+			System.out.println("resolved to: \""+ container_prefix +"\".");			
+		}
+		
 		BPlusTree tree = new BPlusTree(BLOCK_SIZE, 0.5, true);
+		
+		Container container = new BlockFileContainer(container_prefix, BLOCK_SIZE);
+		
+		tree.initialize(getDescriptor, container, 10, 20);
+		
 
-		// tree.
-
-		// tree.
-
+	}
+	
+	public static void main(String[] args) {
+		testLambdas();
 	}
 	
 	private static void endlessLoopToLetJavaRun(){
@@ -36,6 +66,12 @@ public class BPlusInstantiateTest {
 				System.out.println("iteration #" + i + " : " + x);
 			}
 		}
+	}
+	
+	public static void testLambdas() {
+		Function<Integer, Integer> f = (x -> x + 5);
+		System.out.println(f.apply(7));
+		System.out.println((x -> x*x) 17);
 	}
 
 }
